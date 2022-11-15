2a913924fe31ea3af016c1406e7e3a3ed66d36ec
Caroline Vantim <carolvantim@gmail.com>
2022-11-15 12:34:06 -0300

    first commit

---
+++ classes/Login.php
@@ -0,0 +1,52 @@
+<?php
+require_once './banco/Database.php';
+
+class Login extends Database{
+
+    public $email;
+    public $senha;
+
+    public function Cadastrar() {
+        $db = new Database('usuarios');
+        return $db->insert([
+                            'email'=> $this->email,
+                            'senha'  => $this->senha,
+                            'tipo' => $this->tipo
+                        ]);
+    }
+        
+      
+    public function getLoginAluno($email, $senha){
+        
+        $email = filter_input(INPUT_POST, 'email');
+        $senha = filter_input(INPUT_POST, 'senha');
+
+        if(is_null($email) or is_null($senha)){
+            return false;
+        }
+
+        $db = new Database('aluno');
+        return $db->select("email = '$email'");
+
+        if(!$db){
+            return false;
+        }
+
+        if (password_verify($senha, $db['senha'])){
+            unset($db['senha']);
+            $_SESSION['auth'] = $db;
+            return true;
+        }
+        return false;
+
+    }
+
+    public function getProfessor($email){
+        return (new Database('professor'))->select("email = '$email'");
+    }
+
+    public function getEmpresa($email){
+        return (new Database('empresa'))->select("email = '$email'");
+    }
+}
+?>
